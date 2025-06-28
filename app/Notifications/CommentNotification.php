28e4e736e4662e0ad6e015fd337f45c27540be2a
Kitty7997 <125363890+Kitty7997@users.noreply.github.com>
2025-06-28 05:35:03 +0530

    Initial commit

---
+++ app/Notifications/CommentNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CommentNotification extends Notification
+{
+    use Queueable;
+
+    protected $comment;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($comment)
+    {
+       $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('New comment received')
+                    ->line('You received a new comment.')
+                    ->line($this->comment->message)
+                    ->action('View comment', url('/comment'. $this->comment->id))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+           "comment_id" => $this->comment->id,
+           "message" => $this->comment->message,
+        ];
+    }
+}
